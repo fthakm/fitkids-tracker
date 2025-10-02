4c1dbbd8557159108c480555010cfe21cbbd95c9
Muhammad Fatih Akmaludin <109640622+fthakm@users.noreply.github.com>
2025-10-02 21:47:34 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,3 +4,6 @@ const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+console.log("SUPABASE_URL:", process.env.REACT_APP_SUPABASE_URL);
+console.log("SUPABASE_KEY:", process.env.REACT_APP_SUPABASE_ANON_KEY?.slice(0,5) + "...");
