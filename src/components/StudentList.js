828ebe459951e81cfc6cb0858c4420ff03092956
Muhammad Fatih Akmaludin <109640622+fthakm@users.noreply.github.com>
2025-10-03 12:45:05 +0700

    Update StudentList.js

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,55 +1,43 @@
-import React, { useState } from "react";
-import { Box, Button, Select, MenuItem, Typography } from "@mui/material";
-import StudentDetail from "./StudentDetail";
-import AddEditStudentDialog from "../dialogs/AddEditStudentDialog";
+import React from "react";
+import {
+  Table, TableHead, TableBody, TableRow, TableCell,
+  IconButton, Paper, TableContainer
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
 
-export default function StudentList({ students, setStudents, showSnackbar }) {
-  const [filterAge, setFilterAge] = useState("6-8");
-  const [selectedStudent, setSelectedStudent] = useState(null);
-  const [openAddEdit, setOpenAddEdit] = useState(false);
-
-  const handleAdd = () => {
-    setSelectedStudent(null);
-    setOpenAddEdit(true);
-  };
-
+export default function StudentList({ students, onEdit, onDelete, onInput }) {
   return (
-    <Box mt={3}>
-      <Box mb={2} display="flex" justifyContent="space-between">
-        <Select value={filterAge} onChange={e => setFilterAge(e.target.value)}>
-          <MenuItem value="6-8">6-8</MenuItem>
-          <MenuItem value="9-11">9-11</MenuItem>
-          <MenuItem value="12-15">12-15</MenuItem>
-          <MenuItem value="16+">16+</MenuItem>
-        </Select>
-        <Button variant="contained" onClick={handleAdd}>Tambah Siswa</Button>
-      </Box>
-
-      {students.filter(s => s.age === filterAge).map((s) => (
-        <Typography key={s.id || s.name} sx={{ cursor: "pointer", mb: 1 }} onClick={() => setSelectedStudent(s)}>
-          {s.name}
-        </Typography>
-      ))}
-
-      {openAddEdit && (
-        <AddEditStudentDialog
-          open={openAddEdit}
-          onClose={() => setOpenAddEdit(false)}
-          students={students}
-          setStudents={setStudents}
-          student={selectedStudent}
-          showSnackbar={showSnackbar}
-        />
-      )}
-
-      {selectedStudent && (
-        <StudentDetail
-          student={selectedStudent}
-          setStudents={setStudents}
-          showSnackbar={showSnackbar}
-          onClose={() => setSelectedStudent(null)}
-        />
-      )}
-    </Box>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Nama</TableCell>
+            <TableCell>Usia</TableCell>
+            <TableCell>Aksi</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {students?.length > 0 ? (
+            students.map((s) => (
+              <TableRow key={s.id}>
+                <TableCell>{s?.name || "-"}</TableCell>
+                <TableCell>{s?.age || "-"}</TableCell>
+                <TableCell>
+                  <IconButton onClick={() => onEdit(s)}><EditIcon /></IconButton>
+                  <IconButton onClick={() => onDelete(s.id)}><DeleteIcon /></IconButton>
+                  <IconButton onClick={() => onInput(s)}><AddIcon /></IconButton>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={3}>Belum ada siswa</TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
-}
+                         }
