cb5fdaf0f986662c5c8f37671634209cebfdab34
Muhammad Fatih Akmaludin <109640622+fthakm@users.noreply.github.com>
2025-10-03 10:30:45 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,5 +5,9 @@ const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("❌ Supabase ENV not found. Check Netlify settings!");
+}
+
 console.log("SUPABASE_URL:", process.env.REACT_APP_SUPABASE_URL);
 console.log("SUPABASE_KEY:", process.env.REACT_APP_SUPABASE_ANON_KEY?.slice(0,5) + "...");
