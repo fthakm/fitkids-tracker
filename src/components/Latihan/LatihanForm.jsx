714eae6e7a99295425d690a2eecfe2d8a9d61ddc
Muhammad Fatih Akmaludin <109640622+fthakm@users.noreply.github.com>
2025-10-04 14:33:18 +0700

    Create LatihanForm.jsx

---
+++ src/components/Latihan/LatihanForm.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function LatihanForm(){ return (<Box><Typography>Form tambah latihan (pilih hadir, isi nilai)</Typography></Box>); }
