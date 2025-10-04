c12b3dc54a98d2a456e1a50e1202a9717f22d275
Muhammad Fatih Akmaludin <109640622+fthakm@users.noreply.github.com>
2025-10-04 14:33:43 +0700

    Create LatihanHistory.jsx

---
+++ src/components/Latihan/LatihanHistory.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function LatihanHistory(){ return (<Box><Typography>Riwayat latihan</Typography></Box>); }
