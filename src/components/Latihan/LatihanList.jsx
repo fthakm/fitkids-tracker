21e1b6c9ea8473ec575a0e99c4671f537e381b48
Muhammad Fatih Akmaludin <109640622+fthakm@users.noreply.github.com>
2025-10-04 14:33:59 +0700

    Create LatihanList.jsx

---
+++ src/components/Latihan/LatihanList.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function LatihanList(){ return (<Box><Typography variant='h6'>Latihan</Typography></Box>); }
