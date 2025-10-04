59f211a08a0adcdc35f559ccee0acfdc3878a24a
Muhammad Fatih Akmaludin <109640622+fthakm@users.noreply.github.com>
2025-10-04 14:36:15 +0700

    Create StudentForm.jsx

---
+++ src/components/Students/StudentForm.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function StudentForm(){ return (<Box><Typography>Form Siswa (reuse AddEditStudentDialog)</Typography></Box>); }
