e1d92ac2e89d94d01d3456617526ce7ac8f15451
Muhammad Fatih Akmaludin <109640622+fthakm@users.noreply.github.com>
2025-10-05 18:30:14 +0700

    Add files via upload

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,8 @@
+import supabase from 'supabaseClient';
 import { createClient } from '@supabase/supabase-js';
 
-const url = process.env.REACT_APP_SUPABASE_URL;
-const anonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const url = process.env.REACT_APP_SUPABASE_URL || process.env.VITE_SUPABASE_URL || '';
+const anonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || process.env.VITE_SUPABASE_ANON_KEY || '';
 
 const supabase = createClient(url, anonKey);
 
